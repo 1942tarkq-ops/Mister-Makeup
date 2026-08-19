ba9b037c3b57c9c9dcefea7f0eb5631ae477dca6
1942tarkq-ops <1942tarkq@gmail.com>
2026-08-19 15:44:34 +0300

    Update settins.gradle.kts

--- settins.gradle.kts
+++ settins.gradle.kts
@@ -1,19 +1,6 @@
 
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "Mister Makeup"
-include(":app")
